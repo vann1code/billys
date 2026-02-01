fa0a66213910fdf981292d31e8786177d83f7bd6
vann1code <vann1code@gmail.com>
2026-02-01 03:01:35 -0300

    ux/ui

--- src/main/java/br/com/code/billys/model/Usuarios.java
+++ src/main/java/br/com/code/billys/model/Usuarios.java
@@ -1,6 +1,9 @@
 package br.com.code.billys.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,7 +17,18 @@ public class Usuarios {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "O nome não pode estar em branco")
+    @Size(min = 3, max = 100, message = "O nome deve ter entre 3 e 100 caracteres")
+    @Column(nullable = false)
     private String nome;
+
+    @NotBlank(message = "O e-mail é obrigatório")
+    @Email(message = "Informe um e-mail válido (ex: joao@gmail.com)")
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @NotBlank(message = "A senha é obrigatória")
+    @Size(min = 4, message = "A senha deve ter no mínimo 4 caracteres")
+    @Column(name = "senha")
     private String senha;
 }
